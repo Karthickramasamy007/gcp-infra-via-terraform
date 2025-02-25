86e24ee9729c5e78cacd245ecbcc2d16c593c297
Karthickramasamy007 <10cse61@gmail.com>
2025-02-25 17:55:47 +0000

    sf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_subnetwork" "subnet" {
-  name          = "var.subnet_name"
+  name          = var.subnet_name
   region        = var.region
   network       = var.vpc_id
   ip_cidr_range = var.cidr_range
